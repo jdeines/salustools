b8cddde823d9d29a2a9251ecc8ab6d05e4f50e06
Jill Deines <jillian.deines@gmail.com>
2018-06-26 11:33:35 -0700

    added additional water balance variables

--- R/results_processing.R
+++ R/results_processing.R
@@ -40,14 +40,23 @@ salusRawToAnnual <- function(scratchDir, outDir, runname, startYear){
       summarize(GWAD = max(GWAD, na.rm=TRUE),
                 IRRC_cum = max(IRRC, na.rm=TRUE),
                 DRNC_cum = max(DRNC, na.rm=TRUE),
-                PREC_cum = max(PREC, na.rm = TRUE)) %>%
+                PREC_cum = max(PREC, na.rm = TRUE),
+                ETAC_cum = max(ETAC, na.rm=TRUE),
+                ESAC_cum = max(ESAC, na.rm=TRUE),
+                EPAC_cum = max(EPAC, na.rm=TRUE),
+                ROFC_cum = max(ROFC, na.rm=TRUE)) %>%
       # add species name back in
       left_join(cropByYear, by = c('ExpID','Year')) %>%
       # de-cumulate irrc, drnc, prec (turn to annual values)
       mutate(IRRC_mm = IRRC_cum - lag(IRRC_cum, default=0),
              DRNC_mm = DRNC_cum - lag(DRNC_cum, default=0),
-             PREC_mm = PREC_cum - lag(PREC_cum, default=0)) %>%
-      dplyr::select(c(ExpID, Year, SpeciesID, GWAD, IRRC_mm, DRNC_mm, PREC_mm)) %>%
+             PREC_mm = PREC_cum - lag(PREC_cum, default=0),
+             ETAC_mm = ETAC_cum -lag(ETAC_cum, default = 0),
+             ESAC_mm = ESAC_cum -lag(ESAC_cum, default = 0),
+             EPAC_mm = EPAC_cum -lag(EPAC_cum, default = 0),
+             ROFC_mm = ROFC_cum -lag(ROFC_cum, default = 0)) %>%
+      dplyr::select(c(ExpID, Year, SpeciesID, GWAD, IRRC_mm, DRNC_mm, PREC_mm,
+                      ETAC_mm, ESAC_mm, EPAC_mm, ROFC_mm)) %>%
       filter(Year >= startYear)
 
     # add an irrigation flag

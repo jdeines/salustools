f10e15e8b84c0eb4826f1a6f35e66527f2d71b61
Jillian Deines <jillian.deines@gmail.com>
2018-10-09 10:23:00 -0700

    modified county short function to default to all counties in state if county = na

--- R/getNassAPI_funs.R
+++ R/getNassAPI_funs.R
@@ -1,3 +1,45 @@
+#' Extract NASS data from API by custom query components
+#'
+#' Maximum flexibility function to download specified NASS data based on a user supplied
+#' list of parameters. Requires knowledge of Quickstats API parameter codes 
+#' 
+#' @param apiKey Personal key; Obtained from http://quickstats.nass.usda.gov/api
+#' @param params query string and values
+#' @export
+#' @examples
+#' 
+#'  # not yet tested!
+#' apiKey <- '28EAA9E6-8062-34A4-981A-B2ED4692228A' # my code, modified
+#' params = 'source_desc=CENSUS&
+
+getNass <- function(apiKey, params){
+  library(httr)
+  library(jsonlite)
+  # build URL query
+  baseurl <- 'http://quickstats.nass.usda.gov/api/api_GET'
+  format = 'JSON'
+  GETrequest <- paste0(baseurl,
+                       '/?key=',apiKey,
+                        params,
+                       '&format=',format)
+  
+  # if present, replace commas and spaces in url with encodings
+  if(grepl(" ", GETrequest))  GETrequest <- gsub(" ", "%20", GETrequest)
+  if(grepl(",", GETrequest))  GETrequest <- gsub(",", "%2C", GETrequest)
+  
+  # retrive data
+  req <- GET(GETrequest)
+  # check status and throw stop/error: 200 means successful
+  stop_for_status(req)
+  # extract content
+  json <- content(req, as = 'text', encoding = 'UTF-8')
+  # convert from JSON and extract df from list object
+  outputdf <- fromJSON(json)[[1]]
+}
+
+
+
+
 #' Extract NASS data by county from API
 #'
 #' Downloads NASS crop data by requested county, program source, and short desc
@@ -6,7 +48,7 @@
 #' @param program 'CENSUS' or 'SURVEY'
 #' @param shortDesc short description of desired data field; aka 'Data Item'
 #' @param state state 2 digit fips
-#' @param county county 3-digit ANSI code
+#' @param county county 3-digit ANSI code. To get all counties in state, set to NA.
 #' @param year year to start getting data - gets all subsequent years. default is 1996 to get all Census
 #' @keywords NASS county
 #' @export
@@ -41,16 +83,29 @@ getNassCounty_short <- function(apiKey, program, shortDesc, state, county, year 
   # build URL query
   baseurl <- 'http://quickstats.nass.usda.gov/api/api_GET'
   format = 'JSON'
-  GETrequest <- paste0(baseurl,
-                       '/?key=',apiKey,
-                       '&source_desc=',program,
-                       '&short_desc=', shortDesc,
-                       '&agg_level_desc=','COUNTY',
-                       '&state_fips_code=', state,
-                       '&county_ansi=', county,
-                       '&year__GE=',year,
-                       '&format=',format)
   
+  # if county supplied, download by specified counties
+  if(!is.na(county)){
+    GETrequest <- paste0(baseurl,
+                         '/?key=',apiKey,
+                         '&source_desc=',program,
+                         '&short_desc=', shortDesc,
+                         '&agg_level_desc=','COUNTY',
+                         '&state_fips_code=', state,
+                         '&county_ansi=', county,
+                         '&year__GE=',year,
+                         '&format=',format)
+  } else if (is.na(county)){
+    GETrequest <- paste0(baseurl,
+                         '/?key=',apiKey,
+                         '&source_desc=',program,
+                         '&short_desc=', shortDesc,
+                         '&agg_level_desc=','COUNTY',
+                         '&state_fips_code=', state,
+                         '&year__GE=',year,
+                         '&format=',format)
+  }
+    
   # if present, replace commas and spaces in url with encodings
   if(grepl(" ", GETrequest))  GETrequest <- gsub(" ", "%20", GETrequest)
   if(grepl(",", GETrequest))  GETrequest <- gsub(",", "%2C", GETrequest)
